dfdeef81ee81688afc0c6f5e9d3ddc8ee4e21ec1
mindils <mindils@mail.ru>
2024-11-27 17:57:26 +0300

    sql_generator_processor fix bug

--- python_extensions/sql_generator_processor_v1_0_4.py
+++ python_extensions/sql_generator_processor_v1_0_4.py
@@ -2,13 +2,13 @@ from nifiapi.flowfiletransform import FlowFileTransform, FlowFileTransformResult
 import json
 import re
 from jsonpath_ng import parse
-from nifiapi.properties import PropertyDescriptor, StandardValidators
+from nifiapi.properties import PropertyDescriptor
 
 class SQLGeneratorProcessor(FlowFileTransform):
     class Java:
         implements = ['org.apache.nifi.python.processor.FlowFileTransform']
     class ProcessorDetails:
-        version = '1.0.3'
+        version = '1.0.4'
         description = 'Генерирует SQL-запросы, заменяя плейсхолдерами значения из JSON с использованием JSONPath. Позволяет выбрать, куда поместить результат.'
         tags = ['SQL', 'JSON', 'Template']
         dependencies = ['jsonpath-ng']
@@ -24,21 +24,11 @@ class SQLGeneratorProcessor(FlowFileTransform):
             default_value='',
         )
 
-        # Создаем валидатор для Output Destination
-        def output_destination_validator():
-            def validate(subject, value, context):
-                allowable_values = ['flowfile-content', 'flowfile-attribute']
-                if value not in allowable_values:
-                    return False, "Value must be one of {}".format(', '.join(allowable_values))
-                return True, None
-            return validate
-
-        output_destination = PropertyDescriptor(
-            name='Output Destination',
-            description='Определяет, куда поместить сгенерированный SQL: в содержимое FlowFile или в атрибут.',
-            required=True,
-            default_value='flowfile-attribute',
-            validators=[output_destination_validator()]
+        store_in_content = PropertyDescriptor(
+            name='Store in Content',
+            description='Указывает, помещать ли SQL в содержимое FlowFile (true) или в атрибут (false).',
+            required=False,
+            default_value='false',
         )
 
         attribute_name = PropertyDescriptor(
@@ -48,7 +38,7 @@ class SQLGeneratorProcessor(FlowFileTransform):
             default_value='generated_sql',
         )
 
-        return [sql_template, output_destination, attribute_name]
+        return [sql_template, store_in_content, attribute_name]
 
     def transform(self, context, flowfile):
         # Получаем шаблон SQL из свойств процессора
@@ -58,18 +48,13 @@ class SQLGeneratorProcessor(FlowFileTransform):
             self.logger.error('Свойство SQL Template не установлено.')
             return FlowFileTransformResult(relationship='failure')
 
-        # Получаем настройку Output Destination
-        output_destination_prop = context.getProperty('Output Destination')
-        output_destination = output_destination_prop.evaluateAttributeExpressions(flowfile).getValue()
-        if not output_destination:
-            output_destination = 'flowfile-attribute'  # Значение по умолчанию
+        # Получаем настройку Store in Content
+        store_in_content_prop = context.getProperty('Store in Content')
+        store_in_content = store_in_content_prop.evaluateAttributeExpressions(flowfile).getValue().lower() == 'true'
 
         # Если выбран атрибут, получаем имя атрибута
-        if output_destination == 'flowfile-attribute':
-            attribute_name_prop = context.getProperty('Attribute Name')
-            attribute_name = attribute_name_prop.evaluateAttributeExpressions(flowfile).getValue()
-            if not attribute_name:
-                attribute_name = 'generated_sql'  # Значение по умолчанию
+        attribute_name_prop = context.getProperty('Attribute Name')
+        attribute_name = attribute_name_prop.evaluateAttributeExpressions(flowfile).getValue() or 'generated_sql'
 
         # Читаем содержимое FlowFile и парсим JSON
         content_bytes = flowfile.getContentsAsBytes()
@@ -93,23 +78,20 @@ class SQLGeneratorProcessor(FlowFileTransform):
         # Заменяем плейсхолдеры в шаблоне SQL
         sql_generated = re.sub(r'\$\{([^}]+)\}', replace_placeholder, sql_template)
 
-        if output_destination == 'flowfile-content':
+        if store_in_content:
             # Помещаем сгенерированный SQL в содержимое FlowFile
             return FlowFileTransformResult(
                 relationship='success',
                 contents=sql_generated,
                 attributes=flowfile.getAttributes()
             )
-        elif output_destination == 'flowfile-attribute':
+        else:
             # Добавляем сгенерированный SQL в указанный атрибут FlowFile
             attributes = {attribute_name: sql_generated}
             return FlowFileTransformResult(
                 relationship='success',
                 attributes=attributes
             )
-        else:
-            self.logger.error('Недопустимое значение Output Destination: {}'.format(output_destination))
-            return FlowFileTransformResult(relationship='failure')
 
     def evaluate_expression(self, expression, json_data):
         # Парсим JSONPath выражение
